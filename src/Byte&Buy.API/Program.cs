eefdbdaf9de538d5448c56df1a1a1c67b37eb7ee
VortexOoN <wojciechmucha12@gmail.com>
2025-11-03 17:29:04 +0100

    feat: add basic extensions, result types etc

--- src/Byte&Buy.API/Program.cs
+++ src/Byte&Buy.API/Program.cs
@@ -1,18 +1,30 @@
 using Byte_Buy.API.Middleware;
+using Byte_Buy.Core.Extensions;
+using Byte_Buy.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+#region Service Registration & Configuration
 // -----------------------------
 // Api Controllers
 // -----------------------------
 builder.Services.AddControllers();
 
 // -----------------------------
-// Open Api
+// Custom Services & Handlers
+// -----------------------------
+builder.Services
+    .AddCoreLayer()
+    .AddInfrastructureLayer(builder.Configuration);
+
+// -----------------------------
+// Swagger & OpenApi
 // -----------------------------
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+#region Middleware Pipeline
 var app = builder.Build();
 
 // -----------------------------
@@ -23,13 +35,28 @@ app.UseGlobalExceptionHandlingMiddleware();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // -----------------------------
+    // Use Swagger & OpenApi
+    // -----------------------------
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// -----------------------------
+// Https
+// -----------------------------
 app.UseHttpsRedirection();
 
+// --------------------------------
+// Authentication && Authorization
+// --------------------------------
 app.UseAuthorization();
 
+// --------------------------------
+// Map Controllers
+// --------------------------------
 app.MapControllers();
 
 app.Run();
+#endregion
---
+++ src/Byte&Buy.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Byte_Buy.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
