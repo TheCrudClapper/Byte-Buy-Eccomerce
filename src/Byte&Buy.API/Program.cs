c2616575da22a854563abce3d90bf8551eddd6bd
VortexOoN <wojciechmucha12@gmail.com>
2025-11-03 16:22:38 +0100

    feat: add global exception handling middleware

--- src/Byte&Buy.API/Program.cs
+++ src/Byte&Buy.API/Program.cs
@@ -1,34 +1,35 @@
+using Byte_Buy.API.Middleware;
 
-namespace Byte_Buy.API
+var builder = WebApplication.CreateBuilder(args);
+
+
+// -----------------------------
+// Api Controllers
+// -----------------------------
+builder.Services.AddControllers();
+
+// -----------------------------
+// Open Api
+// -----------------------------
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// -----------------------------
+// Global Error Handling
+// -----------------------------
+app.UseGlobalExceptionHandlingMiddleware();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.MapOpenApi();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
