39a5712f51d76e31b5d0537d44e7c2e20c70bade
VortexOoN <wojciechmucha12@gmail.com>
2025-11-24 20:28:32 +0100

    feat(backend&desktop): start adding crud operations

---
+++ Byte&Buy.Desktop/src/ByteBuy.Services/DTO/Employee/EmployeeAddressResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ByteBuy.Core.DTO.Employee;
+
+public record EmployeeAddressResponse(
+    string Street,
+    string HouseNumber,
+    string PostalCode,
+    string City,
+    string Country,
+    string? FlatNumber,
+    string? PhoneNumber
+    );
---
+++ Byte&Buy.Desktop/src/ByteBuy.Services/DTO/Employee/EmployeeAddressUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ByteBuy.Core.DTO.Employee;
+
+public record EmployeeAddressUpdateRequest(
+    [Required, MaxLength(50)] string Street,
+    [Required, MaxLength(10)] string HouseNumber,
+    [Required, MaxLength(20)] string PostalCode,
+    [Required, MaxLength(50)] string City,
+    [Required, MaxLength(50)] string Country,
+    [MaxLength(10)] string? FlatNumber,
+    [MaxLength(15)] string? PhoneNumber
+    );
