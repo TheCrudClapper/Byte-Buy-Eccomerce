8e26dac447bb8b111632c1f78187a7821a00af05
VortexOoN <wojciechmucha12@gmail.com>
2025-12-12 17:59:09 +0100

    feat(backend): add domain rules and validations for item and image

--- Byte&Buy.Backend/src/ByteBuy.Core/Domain/Entities/Image.cs
+++ Byte&Buy.Backend/src/ByteBuy.Core/Domain/Entities/Image.cs
@@ -1,15 +1,16 @@
 ﻿using ByteBuy.Core.Domain.EntityContracts;
+using ByteBuy.Core.ResultTypes;
 
 namespace ByteBuy.Core.Domain.Entities;
 
 public class Image : AuditableEntity, ISoftDeletable
 {
-    public string ImagePath { get; set; } = null!;
-    public Guid ItemId { get; set; }
-    public Item Item { get; set; } = null!;
-    public string AltText { get; set; } = null!;
-    public bool IsActive { get; set; }
-    public DateTime? DateDeleted { get; set; }
+    public string ImagePath { get; private set; } = null!;
+    public Guid ItemId { get; private set; }
+    public Item Item { get; private set; } = null!;
+    public string AltText { get; private set; } = null!;
+    public bool IsActive { get; private set; }
+    public DateTime? DateDeleted { get; private set; }
 
     public void Deactivate()
     {
@@ -17,6 +18,41 @@ public class Image : AuditableEntity, ISoftDeletable
             return;
 
         IsActive = false;
-        DateDeleted = null!;
+        DateDeleted = DateTime.UtcNow;
+    }
+
+    private Image() { }
+    private Image(string imagePath, string altText)
+    {
+        ImagePath = imagePath;
+        AltText = altText;
+        IsActive = true;
+        DateCreated = DateTime.UtcNow;
+    }
+
+    public static Result Validate(string imagePath, string altText)
+    {
+        if(string.IsNullOrWhiteSpace(imagePath))
+            return Result.Failure(Error.Validation("Image Path is required and must be at most 50 characters."));
+
+        if (string.IsNullOrWhiteSpace(altText) || altText.Length > 50)
+            return Result.Failure(Error.Validation("Alernative Text is required and must be at most 50 characters."));
+
+        return Result.Success();
+    }
+
+    internal static Result<Image> Create(string imagePath, string altText)
+    {
+        var validationResult = Validate(imagePath, altText);
+        if (validationResult.IsFailure)
+            return Result.Failure<Image>(validationResult.Error);
+
+        return new Image(imagePath, altText);
+    }
+
+    internal void AssignToItem(Item item) 
+    {
+        ItemId = item.Id;
+        Item = item;
     }
 }
--- Byte&Buy.Backend/src/ByteBuy.Core/Domain/Entities/Item.cs
+++ Byte&Buy.Backend/src/ByteBuy.Core/Domain/Entities/Item.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBuy.Core.Domain.EntityContracts;
+using ByteBuy.Core.ResultTypes;
 
 namespace ByteBuy.Core.Domain.Entities;
 
@@ -16,4 +17,104 @@ public class Item : AuditableEntity, ISoftDeletable
     public bool IsCompanyItem { get; set; }
     public bool IsActive { get; set; }
     public DateTime? DateDeleted { get; set; }
+
+    private Item() { }
+
+    private Item(string name, string description, Category category, Condition condition, int stockQuantity, bool isCompanyItem)
+    {
+        Name = name;
+        Description = description;
+        CategoryId = category.Id;
+        Category = category;
+        ConditionId = condition.Id;
+        Condition = condition;
+        StockQuantity = stockQuantity;
+        IsActive = true;
+        IsCompanyItem = isCompanyItem;
+        DateCreated = DateTime.UtcNow;
+    }
+
+    public void Deactivate()
+    {
+        if (!IsActive)
+            return;
+
+        IsActive = false;
+        DateDeleted = DateTime.UtcNow;
+        DeactivateAllImages();
+    }
+
+    public static Result Validate(string name, string description, int stockQuantity)
+    {
+        if (string.IsNullOrWhiteSpace(name) || name.Length > 75)
+            return Result.Failure(Error.Validation("Name is required and must be at most 75 characters."));
+
+        if (string.IsNullOrWhiteSpace(description) || description.Length > 2000)
+            return Result.Failure(Error.Validation("Description is required and must be at most 2000 characters."));
+
+        if (stockQuantity < 1)
+            return Result.Failure(Error.Validation("Quantity can'tbe lower than 1."));
+
+        return Result.Success();
+    }
+
+    public static Result<Item> Create(string name, string description, Category category, Condition condition, int stockQuantity)
+        => CreateInternal(name, description, category, condition, stockQuantity, false);
+
+    public static Result<Item> CreateCompanyItem(string name, string description, Category category, Condition condition, int stockQuantity)
+        => CreateInternal(name, description, category, condition, stockQuantity, true);
+
+    private static Result<Item> CreateInternal(
+    string name,
+    string description,
+    Category category,
+    Condition condition,
+    int stockQuantity,
+    bool isCompanyItem)
+    {
+        if (condition is null)
+            return Result.Failure<Item>(Error.Validation("Condition can't be null"));
+
+        if (category is null)
+            return Result.Failure<Item>(Error.Validation("Category can't be null"));
+
+        var validationResult = Validate(name, description, stockQuantity);
+        if (validationResult.IsFailure)
+            return Result.Failure<Item>(validationResult.Error);
+
+        return new Item(
+            name,
+            description,
+            category,
+            condition,
+            stockQuantity,
+            isCompanyItem);
+    }
+
+    public Result AddImage(string imagePath, string altText)
+    {
+        var imageResult = Image.Create(imagePath, altText);
+        if(imageResult.IsFailure)
+            return imageResult;
+
+        var image = imageResult.Value;
+        image.AssignToItem(this);
+        Images.Add(image);
+
+        return Result.Success();
+    }
+
+    public void AssignImageToItem(Image image)
+    {
+        image.AssignToItem(this);
+        Images.Add(image);
+    }
+
+    private void DeactivateAllImages()
+    {
+        foreach (Image image in Images)
+        {
+            image.Deactivate();
+        }
+    }
 }
