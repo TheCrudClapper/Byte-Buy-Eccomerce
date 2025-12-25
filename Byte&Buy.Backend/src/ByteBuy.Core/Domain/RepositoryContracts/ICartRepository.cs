a91ee6b1d2314ee37198eb940e7be1e3a1c0c94a
TheCrudClapper <wojciechmucha12@gmail.com>
2025-12-25 02:13:03 +0100

    feat(backend) delete unecessary cart repositories

--- Byte&Buy.Backend/src/ByteBuy.Core/Domain/RepositoryContracts/ICartRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ByteBuy.Core.Domain.Entities;
-using ByteBuy.Core.Domain.RepositoryContracts.Base;
-
-namespace ByteBuy.Core.Domain.RepositoryContracts;
-
-public interface ICartRepository : IRepositoryBase<Cart>
-{
-}
