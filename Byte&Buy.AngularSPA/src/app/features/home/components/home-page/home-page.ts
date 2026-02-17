11ae00585278985392bca5ad96b68c794afa1537
VortexOoN <wojciechmucha12@gmail.com>
2026-02-17 15:48:25 +0100

    feat(angularSPA): add simple home page

--- Byte&Buy.AngularSPA/src/app/features/home/components/home-page/home-page.ts
+++ Byte&Buy.AngularSPA/src/app/features/home/components/home-page/home-page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
+  standalone: true,
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage {
-
+ 
 }
