5eb8c9281a75a6fbb4338eec52415244b04046c4
VortexOoN <wojciechmucha12@gmail.com>
2026-02-17 16:26:25 +0100

    feat(angualarSPA): add footer spinner, remove unused links, options

--- Byte&Buy.AngularSPA/src/app/shared/components/footer/footer.ts
+++ Byte&Buy.AngularSPA/src/app/shared/components/footer/footer.ts
@@ -2,22 +2,35 @@ import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CompanyInfoApiService } from '../../../core/clients/company-info/company-info-api-service';
 import { CompanyInfoResponse } from '../../../core/dto/company/company-info-response';
 
+
 @Component({
   selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.html',
   styleUrls: ['./footer.scss'],
-  imports: [] 
+  imports: []
 })
 
-export class Footer implements OnInit{
+export class Footer implements OnInit {
   private readonly companyInfoApiService = inject(CompanyInfoApiService);
   companyInfo = signal<CompanyInfoResponse | null>(null);
+  isLoading = signal<boolean>(false);
 
-  ngOnInit(){
+  readonly githubLink = "https://github.com/TheCrudClapper";
+  readonly instagramLink = "https://www.instagram.com/muchawojciech/";
+  readonly discordLink = "#";
+
+  ngOnInit() {
+    this.isLoading.set(true);
     this.companyInfoApiService.getCompanyInfo().subscribe({
-        next: (data) => this.companyInfo.set(data),
-        error: (err) => console.error('Error while fetching data', err)
+      next: (data) => {
+        this.companyInfo.set(data);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.error('Error while fetching data', err)
+        this.isLoading.set(false);
+      }
     });
   }
 }
