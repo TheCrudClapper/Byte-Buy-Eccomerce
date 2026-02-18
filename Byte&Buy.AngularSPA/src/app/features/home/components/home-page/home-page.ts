1f8a8941ac9b4e8b3beb410e76a0ee5a97345a8b
VortexOoN <wojciechmucha12@gmail.com>
2026-02-18 16:51:43 +0100

    fix(angualrSPA): fix hero image to use ngSrc

--- Byte&Buy.AngularSPA/src/app/features/home/components/home-page/home-page.ts
+++ Byte&Buy.AngularSPA/src/app/features/home/components/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../../core/clients/auth/auth-service';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, NgOptimizedImage],
   standalone: true,
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
