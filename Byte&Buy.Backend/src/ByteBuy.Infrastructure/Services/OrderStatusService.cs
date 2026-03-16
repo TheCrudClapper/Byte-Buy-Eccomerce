bfa6d3466c0470f8b3f4b38e0cdefd458f947cd4
VortexOoN <wojciechmucha12@gmail.com>
2026-03-16 23:55:55 +0100

    refactor(backend): refactor automatical order cancelation when order is not paid within 24 hours

--- Byte&Buy.Backend/src/ByteBuy.Infrastructure/Services/OrderStatusService.cs
+++ Byte&Buy.Backend/src/ByteBuy.Infrastructure/Services/OrderStatusService.cs
@@ -1,4 +1,5 @@
-﻿using ByteBuy.Core.Domain.Orders.Enums;
+﻿using ByteBuy.Core.Domain.Offers.Base;
+using ByteBuy.Core.Domain.Orders.Enums;
 using ByteBuy.Infrastructure.DbContexts;
 using ByteBuy.Infrastructure.ServiceContracts;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +10,8 @@ public class OrderStatusService : IOrderStatusService
 {
     private readonly ApplicationDbContext _context;
     public OrderStatusService(ApplicationDbContext context)
-       =>  _context = context;
-    
+       => _context = context;
+
     public async Task CancelUnpaidOrders()
     {
         var thresholdDate = DateTime.UtcNow.AddHours(-24);
@@ -19,9 +20,33 @@ public class OrderStatusService : IOrderStatusService
             .Where(o => o.Status == OrderStatus.AwaitingPayment && o.DateCreated <= thresholdDate)
             .ToListAsync();
 
-        foreach(var order in orders)
+        var offerIds = new List<Guid>();
+
+        //Cancel orders
+        foreach (var order in orders)
+        {
             order.ChangeStatus(OrderStatus.SystemCanceled);
-        
+            offerIds.AddRange(order.Lines.Select(line => line.OfferId));
+        }
+
+        var offers = await _context.Offers
+            .Where(o => offerIds.Contains(o.Id))
+            .ToListAsync();
+
+        var offerLookup = offers.ToDictionary(o => o.Id);
+
+        foreach(var order in orders)
+        {
+            foreach(var line in order.Lines)
+            {
+                if(offerLookup.TryGetValue(line.OfferId, out var offer))
+                {
+                    offer.RestoreQuantity(line.Quantity);
+                }
+            }
+        }
+
+        //Restore offer quantity
         await _context.SaveChangesAsync();
     }
 }
