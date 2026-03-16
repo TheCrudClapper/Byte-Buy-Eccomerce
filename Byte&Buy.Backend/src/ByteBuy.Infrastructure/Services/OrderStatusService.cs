1a18a4148f75ecb69a75476f16b1afdd31f50e5b
VortexOoN <wojciechmucha12@gmail.com>
2026-03-17 00:11:21 +0100

    feat(backend): add transactions to order status job

--- Byte&Buy.Backend/src/ByteBuy.Infrastructure/Services/OrderStatusService.cs
+++ Byte&Buy.Backend/src/ByteBuy.Infrastructure/Services/OrderStatusService.cs
@@ -16,37 +16,46 @@ public class OrderStatusService : IOrderStatusService
     {
         var thresholdDate = DateTime.UtcNow.AddHours(-24);
 
+        await using var transaction = await _context.Database.BeginTransactionAsync();
+
         var orders = await _context.Orders
             .Where(o => o.Status == OrderStatus.AwaitingPayment && o.DateCreated <= thresholdDate)
             .ToListAsync();
 
-        var offerIds = new List<Guid>();
+        List<Guid> offerIds = [];
 
-        //Cancel orders
-        foreach (var order in orders)
+        try
         {
-            order.ChangeStatus(OrderStatus.SystemCanceled);
-            offerIds.AddRange(order.Lines.Select(line => line.OfferId));
-        }
-
-        var offers = await _context.Offers
-            .Where(o => offerIds.Contains(o.Id))
-            .ToListAsync();
-
-        var offerLookup = offers.ToDictionary(o => o.Id);
-
-        foreach(var order in orders)
-        {
-            foreach(var line in order.Lines)
+            //Cancel orders
+            foreach (var order in orders)
             {
-                if(offerLookup.TryGetValue(line.OfferId, out var offer))
+                order.ChangeStatus(OrderStatus.SystemCanceled);
+                offerIds.AddRange(order.Lines.Select(line => line.OfferId));
+            }
+
+            //Restore offer quantity
+            var offers = await _context.Offers
+                .Where(o => offerIds.Contains(o.Id))
+                .ToListAsync();
+
+            var offerLookup = offers.ToDictionary(o => o.Id);
+
+            foreach (var order in orders)
+            {
+                foreach (var line in order.Lines)
                 {
-                    offer.RestoreQuantity(line.Quantity);
+                    if (offerLookup.TryGetValue(line.OfferId, out var offer))
+                    {
+                        offer.RestoreQuantity(line.Quantity);
+                    }
                 }
             }
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
         }
-
-        //Restore offer quantity
-        await _context.SaveChangesAsync();
+        catch
+        {
+            await transaction.RollbackAsync();
+        }
     }
 }
